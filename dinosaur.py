e885247b81c332169e6baea3b18c181f4f6d1421
unknown <rknowles12301@gmail.com>
2023-03-03 05:15:02 -0800

    changed dinosaur class from dinosaur attacking itself and attacking the robot.

--- dinosaur.py
+++ dinosaur.py
@@ -4,6 +4,7 @@ class Dinosaur():
         self.attack_power = 85
         self.health = 200
 
+# make dinosaur attack robot instead of itself
     def attack(self, robot):
         robot.health -= self.attack_power  
         
